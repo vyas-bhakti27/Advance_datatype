7b6ffe6165b5385c57b01eef10f269dac396ee02
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-06-02 12:25:42 +0530

    Create Ad_data2.c

---
+++ Ad_data2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+        int i,sum=0,size=0;
+        printf("Enter size of the array: "); // enter size of array
+        scanf("%d", &size);
+
+        int arr[size];
+
+        printf("Enter %d elements in the array: ",size); // enter elements of array
+
+        for(i=0;i<size;i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        for(i=0;i<size;i++)
+        {
+            sum = sum + arr[i]; // calculate sum of array
+
+        }
+
+        printf("\nSum of all elements of array = %d",sum);
+        return 0;
+}
